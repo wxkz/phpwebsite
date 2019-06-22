708701308862e8f8c27dd753362aae4244116d4b
wxkz <wxkzwin@gmail.com>
2019-06-22 13:55:16 -0300

    weather/index.php fixed

--- weather/index.php
+++ weather/index.php
@@ -1,98 +1,97 @@
 <?php
-
-// VARIAVEIS DA API E DA SAIDA JSON
-$apikey = "";
+// Api configs and json return
+$apikey = "f1a57ea2";
 $json = file_get_contents("https://api.hgbrasil.com/weather?fields=only_results,temp,description,city_name,forecast,max,min,date,humidity,condition,time&key=" . $apikey . "&user_ip=remote");
 $data = json_decode($json,true);
 
-// PEGANDO INFORMAÇOES CLIMATICAS DO DIA ATUAL
-// E SETANDO VARIAVEIS
+// Getting climatic information on the current day
+// and Setting variables
 $daynow =$data['date'];
 $city =$data['city_name'];
 $temp =$data['temp'];
 $description =$data['description'];
+// Set image size
+$tamanho = 60;
 
-// VAR QUE VAI ARMAZENAR A IMAGEM QUE SERA MOSTRADA
+// Var that will store the image that will be shown
 $imgid =1;
 $imgdy = $imgid . ".png";
 
-// CONTATENANDO DADOS DO DIA ATUAL
+// Contacts current day data
 $statusnow ="Cidade: " . $city . "\n" . "Temperatura: " . $temp;
 
+$daysInfo = array ();
+for ($i = 0; $i < 9; $i++) {
+	// Setting up day information
+	// 0 date | 1 min | 2 max | 3 desc | 4 img
+	$daysInfo[$i][0] = $data['forecast'][$i]['date'];
+	$daysInfo[$i][1] = $data['forecast'][$i]['min'];
+	$daysInfo[$i][2] = $data['forecast'][$i]['max'];
+	$daysInfo[$i][3] = $data['forecast'][$i]['condition'];
 
-// SETANDO INFORMAÇOES DOS DIAS
-$date[1] = $data['forecast']['0']['date'];
-$min[1] = $data['forecast']['0']['min'];
-$max[1] = $data['forecast']['0']['max'];
-$description[1] = $data['forecast']['0']['condition'];
-
-// SETANDO TAMANHO DAS IMAGENS
-$tamanho = 60;
-
-// SETANDO IMAGEM DEPENDENDO DA CONDIÇÃO CLIMATICA
-$c = 2;
-for ($i = 1; $i < 9; $i++) {
-	if ($description[i] == "storm"){
-		$imgid =7;
-		$imgdy = $imgid . ".png";
-		$description[i] ="Tempestade";
-	}
-	elseif ($description[i] == "hail"){
-		$imgid =6;
-		$imgdy = $imgid . ".png";
-		$description[i] ="Granizo";
-	}
-	elseif ($description[i] == "rain"){
-		$imgid =5;
-		$imgdy = $imgid . ".png";
-		$description[i] ="Chuva";
-	}
-	elseif ($description[i] == "cloud" or "cloudly_day" or "cloudly_night"){
-		$imgid =3;
-		$imgdy = $imgid . ".png";
-		$description[i] ="Nublado";
-	}
-	elseif ($description[i] == "fog"){
-		$imgid =2;
-		$imgdy = $imgid . ".png";
-		$description[i] ="Neblina";
-	}
-	elseif ($description[i] == "clear_day" or "clear_night"){
-		$imgid =1;
-		$imgdy = $imgid . ".png";
-		$description[i] ="Dia Limpo";
-	}
-	elseif ($description[i] == "none_day" or "none_night"){
-		$imgid =2;
-		$imgdy = $imgid . ".png";
-		$description[i] ="Falha Na Comunicação Com A Api";
-	}
-	$img[i] = "<img src=./img/$imgdy width=$tamanho height=$tamanho>";
-
-	$date[c] =$data['forecast'][$i]['date'];
-	$min[c] =$data['forecast'][$i]['min'];
-	$max[c] =$data['forecast'][$i]['max'];
-	$description[c] =$data['forecast'][$i]['condition'];
-	$c += 1;
+// Setting image depending on climatic condition
+if ($daysInfo[$i][3] == "storm"){
+	$imgid =7;
+	$imgdy = $imgid . ".png";
+	$daysInfo[$i][3] ="Tempestade";
 }
-// CONTATENANDO INFORMAÇOES DOS DIAS
-for ($i = 1; i < 9; $i++){
-	$status[i] += 
-	$img[i] . "\n" . $date[i] . "\n" . $description[i] . "\n" . "Minima: " . $min[i] . "\n" . "Maxima: " . $max[i] . "\n";
+elseif ($daysInfo[$i][3] == "hail"){
+	$imgid =6;
+	$imgdy = $imgid . ".png";
+	$daysInfo[$i][3] ="Granizo";
+}
+elseif ($daysInfo[$i][3] == "rain"){
+	$imgid =5;
+	$imgdy = $imgid . ".png";
+	$daysInfo[$i][3] ="Chuva";
+}
+elseif ($daysInfo[$i][3] == "cloud" or "cloudly_day" or "cloudly_night"){
+	$imgid =3;
+	$imgdy = $imgid . ".png";
+	$daysInfo[$i][3] ="Nublado";
+}
+elseif ($daysInfo[$i][3] == "fog"){
+	$imgid =2;
+	$imgdy = $imgid . ".png";
+	$daysInfo[$i][3] ="Neblina";
+}
+elseif ($daysInfo[$i][3] == "clear_day" or "clear_night"){
+	$imgid =1;
+	$imgdy = $imgid . ".png";
+	$daysInfo[$i][3] ="Dia Limpo";
+}
+elseif ($daysInfo[$i][3] == "none_day" or "none_night"){
+	$imgid =2;
+	$imgdy = $imgid . ".png";
+	$daysInfo[$i][3] ="Falha Na Comunicação Com A Api";
 }
 
-// PRINTANDO RESULTADO
-// CSS STYLESHEET <_<
-$displaystatusnow = "<div id=\"statusnow\"><h3>$statusnow</h3></div>";
-$display = "<div class=\"wrapper\">";
-for ($i = 1; $i < 9;$i++){
-	$display += "<div class=\"item".$i."\">".$status[i]."</div>";
-}
-$display += "</div>";
+// Image representing the climatic condition of the day
+$daysInfo[$i][4] = "<img src=./img/$imgdy width=$tamanho height=$tamanho>";
 
+}
+
+
+$daysInfoFormatedString = array();
+for ($i = 0; $i < 9; $i++) {
+	$daysInfoFormatedString[$i] = $daysInfo[$i][4] . "\n" . $daysInfo[$i][0] . "\n" . $daysInfo[$i][3] . "\n" . "Minima: " . $daysInfo[$i][1] . "\n" . "Maxima: " . $daysInfo[$i][2] . "\n";
+}
+
+$statusnowFormatedString = "<div id=\"statusnow\"><h3>$statusnow</h3></div>";
+
+$daysInfoIframeReady = "<div class=\"wrapper\">
+<div class=\"item1\"> $daysInfoFormatedString[0] </div>
+<div class=\"item2\"> $daysInfoFormatedString[1] </div>
+<div class=\"item3\"> $daysInfoFormatedString[2] </div>
+<div class=\"item4\"> $daysInfoFormatedString[3] </div>
+<div class=\"item5\"> $daysInfoFormatedString[4] </div>
+<div class=\"item6\"> $daysInfoFormatedString[5] </div>
+<div class=\"item7\"> $daysInfoFormatedString[6] </div>
+<div class=\"item8\"> $daysInfoFormatedString[7] </div>
+<div class=\"item9\"> $daysInfoFormatedString[8] </div>
+</div>";
 
 $css = "<style>
-
 /* DESKTOP ############################################################################################### */
 @media screen and (min-width: 768px) {
 	h4 {
@@ -115,10 +114,7 @@ $css = "<style>
 		  \"g g g g h h h h i i i i\";
 	  }
 }
-
-
 /* MOBILE ############################################################################################### */
-
 @media screen and (min-width: 0px) and (max-width: 768px) {
 	h4 {
 		text-align: right;
@@ -127,7 +123,6 @@ $css = "<style>
 		display: grid;
 		grid-template-columns: 500px auto auto;
 	}
-
 	.wrapper {
 		display: grid;
 		grid-auto-columns: 45px;
@@ -202,7 +197,8 @@ $css = "<style>
 }
 </style>";
 
-
 echo "<pre>";
-echo $displaystatusnow, $display, $css;
+echo $statusnowFormatedString, $daysInfoIframeReady, $css;
 ?>
+
+
